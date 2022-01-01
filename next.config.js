d450ef38274b32af8778ee00f82a46c9b295cfc6
Ben Carlson <bjcarlson42@outlook.com>
2022-01-01 18:21:17 -0500

    display -> standalone

--- next.config.js
+++ next.config.js
@@ -1,4 +1,5 @@
 const withPWA = require("next-pwa");
+const runtimeCaching = require('next-pwa/cache')
 
 module.exports = withPWA({
   reactStrictMode: true,
@@ -7,5 +8,6 @@ module.exports = withPWA({
     register: true,
     skipWaiting: true,
     disable: process.env.NODE_ENV === "development",
+    runtimeCaching
   },
 });
